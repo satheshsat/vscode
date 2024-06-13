7ca2ca14bd3c676e6354da2d70a737cd510b1884
Sathesh kumar <satheshs.sat@gmail.com>
2024-06-13 16:21:21 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start code-server
-exec /usr/bin/code-server --bind-addr 0.0.0.0:8080 "$@"
+exec /usr/bin/code-server --bind-addr 0.0.0.0:8080 --auth password --password sathesh@123 "$@"
